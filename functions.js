1a85ab7efc33db02e54bbfb8dd4c4f430bfc426e
dkingdon <daniel.kingdon@gmail.com>
2016-10-28 13:32:15 -0700

    completed assignment 1 and 2.

--- functions.js
+++ functions.js
@@ -1,1 +1,23 @@
 // TODO: add your solutions here!
+
+
+// -------- Assignment 1 --------------------------------
+function combineWords(word1, word2){
+    console.log(word1 + word2);
+    return word1 + word2;
+}
+
+var result = combineWords('dog', 'house');
+
+console.log(result);
+
+// -------- Assignment 2 --------------------------------
+function repeatPhrase(phrase, n){
+    for (var i = 0; i < n; i++){
+        console.log(phrase);
+    }
+}
+
+repeatPhrase('Hello', 5);
+
+// -------- Assignment 3 --------------------------------
